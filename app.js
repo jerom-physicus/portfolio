d472626929dc8d7c75c2905935cb05ba2e9f26b3
jeremiah <114723077+jerom-physicus@users.noreply.github.com>
2023-08-17 21:10:08 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -20,9 +20,45 @@ let target3 = document.querySelector(".page1-h");
 let target4 = document.querySelector(".page1-p");
 let target5 = document.querySelector(".profile-img2");
 let target6 = document.querySelector(".about-h");
+let target7 = document.querySelector(".about-box");
+let target8 = document.querySelector(".socials-page-1");
 observer.observe(target);
 observer.observe(target2);
 observer.observe(target3);
 observer.observe(target4);
 observer.observe(target5);
 observer.observe(target6);
+observer.observe(target7);
+observer.observe(target8);
+
+const observer2 = new IntersectionObserver(handleIntersection2);
+function handleIntersection2(entries) {
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show2");
+    } else {
+      entry.target.classList.remove("show2");
+    }
+  });
+}
+
+let target21 = document.querySelector(".logos");
+observer2.observe(target21);
+
+const observer3 = new IntersectionObserver(handleIntersection3);
+function handleIntersection3(entries) {
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show3");
+    } else {
+      entry.target.classList.remove("show3");
+    }
+  });
+}
+
+let target31 = document.querySelector(".project-1");
+let target32 = document.querySelector(".project-2");
+let target33 = document.querySelector(".project-3");
+observer3.observe(target31);
+observer3.observe(target32);
+observer3.observe(target33);
