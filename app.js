a30eebe2f828eb1222236408ef038ebbafacb0c5
jeremiah <114723077+jerom-physicus@users.noreply.github.com>
2023-08-18 14:32:20 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -42,14 +42,6 @@ observer.observe(target7);
 observer.observe(target8);
 observer.observe(target9);
 observer.observe(target10);
-//target2.forEach((el) => observer.observe(el));
-//target3.forEach((el) => observer.observe(el));
-//target5.forEach((el) => observer.observe(el));
-//target6.forEach((el) => observer.observe(el));
-//target7.forEach((el) => observer.observe(el));
-//target8.forEach((el) => observer.observe(el));
-//target9.forEach((el) => observer.observe(el));
-//target10.forEach((el) => observer.observe(el));
 
 const observer2 = new IntersectionObserver(handleIntersection2);
 function handleIntersection2(entries) {
@@ -90,3 +82,55 @@ button.addEventListener("click", function () {
   mode.classList.toggle("active");
   mode2.classList.toggle("active2");
 });
+
+const observerD = new IntersectionObserver(handleIntersectionD);
+function handleIntersectionD(entries) {
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+}
+
+let targetD = document.querySelector(".profile-img-D");
+let targetD2 = document.querySelector(".more-btn-D ");
+let targetD3 = document.querySelector(".page1-h-D");
+let targetD4 = document.querySelector(".page1-p-D");
+let targetD5 = document.querySelector(".profile-img2-D");
+let targetD6 = document.querySelector(".about-h-D");
+let targetD7 = document.querySelector(".about-box-D");
+let targetD8 = document.querySelector(".socials-page-1-D");
+let targetD9 = document.querySelector(".exp-h-D");
+let targetD10 = document.querySelector(".projects-h1-D");
+let targetD11 = document.querySelector(".logos-D");
+observerD.observe(targetD);
+observerD.observe(targetD2);
+observerD.observe(targetD3);
+observerD.observe(targetD4);
+observerD.observe(targetD5);
+observerD.observe(targetD6);
+observerD.observe(targetD7);
+observerD.observe(targetD8);
+observerD.observe(targetD9);
+observerD.observe(targetD10);
+observerD.observe(targetD11);
+
+const observer3D = new IntersectionObserver(handleIntersection3D);
+function handleIntersection3D(entries) {
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show3");
+    } else {
+      entry.target.classList.remove("show3");
+    }
+  });
+}
+
+let target31D = document.querySelector(".project-1-D");
+let target32D = document.querySelector(".project-2-D");
+let target33D = document.querySelector(".project-3-D");
+observer3D.observe(target31D);
+observer3D.observe(target32D);
+observer3D.observe(target33D);
