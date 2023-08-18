6af50986afec3648caecab89f01ec7cb70a2676f
jeremiah <114723077+jerom-physicus@users.noreply.github.com>
2023-08-18 11:16:32 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 document.querySelector(".menu-icon").addEventListener("click", function () {
   document.getElementById("nav").style.transform = "translateY(0%)";
 });
+document.querySelector(".menu-icon2").addEventListener("click", function () {
+  document.getElementById("nav2").style.transform = "translateY(0%)";
+});
 document.querySelector(".close-icon").addEventListener("click", function () {
   document.getElementById("nav").style.transform = "translateY(-100%)";
+  document.getElementById("nav2").style.transform = "translateY(-100%)";
+});
+document.getElementById("close2").addEventListener("click", function () {
+  document.getElementById("nav2").style.transform = "translateY(-100%)";
 });
 const observer = new IntersectionObserver(handleIntersection);
 function handleIntersection(entries) {
-  entries.map((entry) => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     } else {
@@ -14,26 +21,25 @@ function handleIntersection(entries) {
     }
   });
 }
-let target = document.querySelector(".profile-img");
-let target2 = document.querySelector(".more-btn ");
-let target3 = document.querySelector(".page1-h");
-let target4 = document.querySelector(".page1-p");
-let target5 = document.querySelector(".profile-img2");
+let target = document.querySelectorAll(".profile-img");
+//let target2 = document.querySelector(".more-btn ");
+//let target3 = document.querySelector(".page1-h");
+//let target4 = document.querySelector(".page1-p");
+//let target5 = document.querySelector(".profile-img2");
 let target6 = document.querySelector(".about-h");
 let target7 = document.querySelector(".about-box");
 let target8 = document.querySelector(".socials-page-1");
 let target9 = document.querySelector(".exp-h");
 let target10 = document.querySelector(".projects-h1");
-observer.observe(target);
-observer.observe(target2);
-observer.observe(target3);
-observer.observe(target4);
-observer.observe(target5);
-observer.observe(target6);
-observer.observe(target7);
-observer.observe(target8);
-observer.observe(target9);
-observer.observe(target10);
+target.forEach((el) => observer.observe(el));
+//target2.forEach((el) => observer.observe(el));
+//target3.forEach((el) => observer.observe(el));
+//target5.forEach((el) => observer.observe(el));
+//target6.forEach((el) => observer.observe(el));
+//target7.forEach((el) => observer.observe(el));
+//target8.forEach((el) => observer.observe(el));
+//target9.forEach((el) => observer.observe(el));
+//target10.forEach((el) => observer.observe(el));
 
 const observer2 = new IntersectionObserver(handleIntersection2);
 function handleIntersection2(entries) {
